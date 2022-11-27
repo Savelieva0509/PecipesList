2bae50f5aba35b0d51736c608c80b26fda690d08
Babenko Liudmyla <savelieva0509@gmail.com>
2022-11-27 21:38:58 +0200

    стилизация

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
 
 import recipes from 'recipes.json';
 import { RecipeList } from './RecipeList';
+import { Container } from './App.styled'
 
 
 export const App = () => {
   return (
-    <div>
+    <Container>
       <RecipeList recipes = {recipes} />
-    </div>
-      
-   
+    </Container>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = {
+  colors: {
+    white: "white",
+    black: "black",
+    green: "green",
+    red: "red",
+    blue: "blue",
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
